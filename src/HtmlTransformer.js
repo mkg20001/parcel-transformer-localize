14fb35b260bc090fcbd4839bc6518dadba24e925
Maciej Krüger <mkg20001@gmail.com>
2019-09-07 15:38:25 +0200

    feat: continue transformer work

--- src/HtmlTransformer.js
+++ src/HtmlTransformer.js
@@ -4,7 +4,20 @@ import nullthrows from 'nullthrows'
 import render from 'posthtml-render'
 import semver from 'semver'
 
+const defaults = {
+  sourceLanguage: 'en',
+  translationPath: './l10n',
+  autoAdd: true,
+  autoRemove: true
+}
+
 export default new Transformer({
+  async getConfig ({asset}) {
+    const config =
+      (await asset.getConfig(['.localize', '.localize.js', 'localize.config.js'])) || {}
+    return config
+  },
+
   canReuseAST ({ast}) {
     return ast.type === 'posthtml' && semver.satisfies(ast.version, '^0.4.0')
   },
@@ -19,10 +32,19 @@ export default new Transformer({
     }
   },
 
-  transform ({asset}) {
-    // Handle .htm
-    asset.type = 'html'
-    console.log(asset.ast)
+  async transform ({asset, config, options}) {
+    // TODO: don't mess with translation files in production/build, only dev
+
+    for (const key in defaults) {
+      if (typeof config[key] === 'undefined') { // apply defaults
+        config[key] = defaults[key]
+      }
+    }
+
+    const isProd = options.production
+    const translationDir = null // TODO: add
+
+    const files = await asset.fs.readdirSync(translationDir)
 
     return [asset]
   },
